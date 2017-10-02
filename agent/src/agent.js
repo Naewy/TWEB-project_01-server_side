0e45a9c52def667a9c1f1d5e696194982cfc7e8f
Naewy <katomey@gmail.com>
2017-10-02 11:56:46 +0200

    Data recuperation done, post left to do

--- agent/src/agent.js
+++ agent/src/agent.js
@@ -5,7 +5,7 @@ class Agent {
     this.credentials = credentials;
   }
 
-  fetchAndProcessAllRepos(owner, allReposAreAvailable) {
+  fetchAndProcessAllRepos(owner, nbRepoToKeep, allReposAreAvailable) {
     const targetUrl = `https://api.github.com/orgs/${owner}/repos?state=all&per_page=100`;
     let repos = [];
     function fetchAndProcessPage(pageUrl, credentials) {
@@ -14,17 +14,32 @@ class Agent {
         .get(pageUrl)
         .auth(credentials.username, credentials.token)
         .end((err, res) => {
-          repos = repos.concat(res.body);
+          res.body.forEach((item) => {
+            repos = repos.concat({ id: item.id, name: item.name, nb_issues: item.open_issues });
+          });
           if (res.links.next) {
             fetchAndProcessPage(res.links.next, credentials);
           } else {
-            console.log(repos.length);
-            allReposAreAvailable(null, repos);
+            let topRepos = [];
+            repos.sort((elem1, elem2) => elem2.nb_issues - elem1.nb_issues);
+            for (let i = 0; i < nbRepoToKeep; i += 1) {
+              topRepos = topRepos.concat(repos[i]);
+            }
+            allReposAreAvailable(null, topRepos);
           }
         });
     }
     fetchAndProcessPage(targetUrl, this.credentials);
   }
+
+  /* TODO asynch ou pas? Faut que je voie encore avec Liechti...
+  postTheData(data, url, dataArePosted) {
+    console.log('Posting data to the server');
+    request
+      .post(url)
+      .data(data);
+    dataArePosted();
+  } */
 }
 
 module.exports = Agent;
--- agent/test/agent.js
+++ agent/test/agent.js
@@ -8,7 +8,7 @@ describe('The agent', () => {
   it('should fetch repos', (done) => {
     const owner = 'Microsoft';
     const agent = new Agent(credentials);
-    agent.fetchAndProcessAllRepos(owner, (err, repos) => {
+    agent.fetchAndProcessAllRepos(owner, 10, (err, repos) => {
       should.not.exist(err);
       repos.should.be.an('array');
       done();
